cbfdd59ab18295c57e54878d545ef123c0beb7b0
Christian Sandven <christian.sandven@outlook.com>
2026-06-29 09:13:09 +0200

    Sync script

---
+++ sync.sh
@@ -0,0 +1,6 @@
+cp client.gd example/addons/indigauge/client.gd
+cp core.gd example/addons/indigauge/core.gd
+cp types.gd example/addons/indigauge/types.gd
+cp feedback_panel.gd example/addons/indigauge/feedback_panel.gd
+cp feedback_panel.tscn example/addons/indigauge/feedback_panel.tscn
+cp plugin.cfg example/addons/indigauge/plugin.cfg
