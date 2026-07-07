04be47770397fd1b879fffc00ef853e18c242886
Christian Sandven <christian.sandven@outlook.com>
2026-07-07 09:49:09 +0200

    Prepare for asset store (#6)
    * Git ignore and git attributes
    
    * Addon folder to match structure
    
    * Fix release action

--- sync.sh
+++ sync.sh
@@ -1,6 +1,1 @@
-cp client.gd example/addons/indigauge/client.gd
-cp core.gd example/addons/indigauge/core.gd
-cp types.gd example/addons/indigauge/types.gd
-cp feedback_panel.gd example/addons/indigauge/feedback_panel.gd
-cp feedback_panel.tscn example/addons/indigauge/feedback_panel.tscn
-cp plugin.cfg example/addons/indigauge/plugin.cfg
+cp -r addons example
